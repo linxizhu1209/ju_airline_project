765e140c3673b9338ca4b2e15a32b3e1de10717e
linxizhu1209 <146171215+linxizhu1209@users.noreply.github.com>
2025-01-05 23:15:14 +0900

    feat: 네이버 oauth2 구현
    - 네이버로 로그인하기도 추가

--- src/main/java/org/example/paymenttest/service/CustomOAuth2UserService.java
+++ src/main/java/org/example/paymenttest/service/CustomOAuth2UserService.java
@@ -8,6 +8,8 @@ import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
+
 @Service
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
     @Autowired
@@ -15,12 +17,19 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         OAuth2User oAuth2User = super.loadUser(userRequest);
-        String email = oAuth2User.getAttribute("email");
-        String name = oAuth2User.getAttribute("name");
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+        Map<String, Object> attributes = oAuth2User.getAttributes();
+        String email = null;
+        String name = null;
 
-        // todo 만약 유저테이블에 이미 있는 사람이라면 바로 로그인되게끔하고,
-        // todo 유저테이블에 없는 사람이라면, 해당 유저정보를 가지고 비밀번호 등 입력하여 회원가입하는 곳으로 이동하게 함
-        // todo 이후 회원가입이 완료되면 다시 로그인해주세요!라는 창을 띄우고, 구글 로그인을 하면 유저테이블에 있을테니까 로그인 성공
+        if("google".equals(registrationId)){
+            email = (String) attributes.get("email");
+            name = (String) attributes.get("name");
+        } else if("naver".equals(registrationId)){
+            Map<String,Object> response = (Map<String, Object>) attributes.get("response");
+            email = (String) response.get("email");
+            name = (String) response.get("name");
+        }
 
         userService.saveUser(email,name);
         return oAuth2User;
